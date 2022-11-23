d3671756f38a6ba868f63ce5f2ce6ea4946f2b05
StardustSeemsInk <114210472+StardustSeemsInk@users.noreply.github.com>
2022-11-23 20:57:10 +0800

    💾 Feat: 部分地实现了拖入kxp时导入插件

--- Views/Pages/RepoPage.axaml.cs
+++ Views/Pages/RepoPage.axaml.cs
@@ -1,6 +1,13 @@
-﻿using Avalonia.Controls;
+using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 using KitX_Dashboard.ViewModels.Pages;
+using System.Threading;
+using Serilog;
+using System;
+using KitX_Dashboard.Services;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace KitX_Dashboard.Views.Pages
 {
@@ -18,7 +25,40 @@ namespace KitX_Dashboard.Views.Pages
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+
+            AddHandler(DragDrop.DropEvent, Drop);
+            AddHandler(DragDrop.DragOverEvent, DragOver);
         }
+
+        private void DragOver(object sender, DragEventArgs e)
+        {
+            // Only allow Copy or Link as Drop Operations.
+            e.DragEffects = e.DragEffects & (DragDropEffects.Copy | DragDropEffects.Link);
+
+            // Only allow if the dragged data contains text or filenames.
+            if (!e.Data.Contains(DataFormats.FileNames))
+                e.DragEffects = DragDropEffects.None;
+        }
+
+        private void Drop(object sender, DragEventArgs e)
+        {
+            string[]? files = e.Data.GetFileNames().ToArray();
+            if (files != null && files?.Length > 0)
+            {
+                new Thread(() =>
+                {
+                    try
+                    {
+                        PluginsManager.ImportPlugin(files, true);
+                    }
+                    catch (Exception ex)
+                    {
+                        Log.Error("In RepoPage.Drop()", ex);
+                    }
+                }).Start();
+            }
+        }
+
     }
 }
 
