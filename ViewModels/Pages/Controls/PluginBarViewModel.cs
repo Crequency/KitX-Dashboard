3667e6bf96b6381646eb27673428d7259f66a0fc
StardustSeemsInk <114210472+StardustSeemsInk@users.noreply.github.com>
2022-12-05 07:32:57 +0800

    🔧 Fix: 修复Log不显示详细报错信息的问题

--- ViewModels/Pages/Controls/PluginBarViewModel.cs
+++ ViewModels/Pages/Controls/PluginBarViewModel.cs
@@ -168,7 +168,7 @@ namespace KitX_Dashboard.ViewModels.Pages.Controls
                 }
                 catch (Exception ex)
                 {
-                    Log.Error("In PluginBarViewModel.Launch()", ex);
+                    Log.Error(ex, "In PluginBarViewModel.Launch()");
                 }
             }).Start();
         }
