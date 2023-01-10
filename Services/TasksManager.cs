582eab2bbac138318cd95575fdfff61d5dc3aed6
Dynesshely <catrol@qq.com>
2023-01-10 21:03:31 +0800

    💾 📝 Feat, Chore: 简单的信号槽机制的任务管理器; 更新 Common.BasicHelper 来扩展更多 Queue 的用法

---
+++ Services/TasksManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.BasicHelper.Util.Extension;
+
+namespace KitX_Dashboard.Services
+{
+    internal class TasksManager
+    {
+        private readonly Dictionary<string, Queue<Action>> SignalTasks = new();
+
+        /// <summary>
+        /// 触发信号
+        /// </summary>
+        /// <param name="signal">信号名称</param>
+        internal void RaiseSignal(string signal)
+        {
+            if (SignalTasks.ContainsKey(signal))
+            {
+                var queue = SignalTasks[signal];
+                queue.ForEach(x => x.Invoke());
+                SignalTasks.Remove(signal);
+            }
+        }
+
+        /// <summary>
+        /// 信号发生时运行任务
+        /// </summary>
+        /// <param name="signal">信号</param>
+        /// <param name="action">任务</param>
+        internal void SignalRun(string signal, Action action)
+        {
+            if (SignalTasks.ContainsKey(signal))
+                SignalTasks[signal].Enqueue(action);
+            else SignalTasks.Add(signal, new Queue<Action>().Push(action));
+        }
+    }
+}
