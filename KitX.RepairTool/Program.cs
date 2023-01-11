65b06c6d8217e434596415303cd8eb77c5e8e303
Dynesshely <catrol@qq.com>
2023-01-11 15:21:00 +0800

    💾 📝 Feat, Chore: 初始化 KitX.RepairTool 项目

---
+++ KitX.RepairTool/Program.cs
@@ -0,0 +1,13 @@
+﻿
+var tip_copyright = () =>
+{
+    Console.WriteLine(
+        $"""
+        KitX Repair Tool (C) Crequency
+        Environment: {Environment.Version}
+        OS Version: {Environment.OSVersion}
+        """);
+};
+
+tip_copyright();
+
