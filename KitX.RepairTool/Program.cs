275ecb989e7725880a931bdb7bd8ff2d03c43254
Dynesshely <catrol@qq.com>
2023-01-11 15:49:19 +0800

    💾 Feat: 新增在 linux 平台设定 `LC_ALL=C` 环境变量的 wayland 修复方案

--- KitX.RepairTool/Program.cs
+++ KitX.RepairTool/Program.cs
@@ -9,5 +9,49 @@ var tip_copyright = () =>
         """);
 };
 
+var log_exception = (Exception e) =>
+{
+    Console.WriteLine(e.Message);
+    Console.WriteLine(e.StackTrace);
+};
+
+T? ask<T>(string tip = "Input: ", Func<string?, T>? parse = null)
+{
+    Console.Write(tip);
+    string? input = Console.ReadLine();
+    if (input is null) return default(T);
+    if (parse is not null) return parse(input);
+    else throw new Exception(input);
+};
+
+var menu = () =>
+{
+    Console.WriteLine(
+        """
+        1. (root) Linux wayland repair (add `LC_ALL=C` to environment variables)
+        """);
+    return ask("Your select: ", x => int.TryParse(x, out int y) ? y : -1);
+};
+
 tip_copyright();
+switch (menu())
+{
+    case 1:
+        if (!OperatingSystem.IsLinux())
+        {
+            Console.WriteLine("Only on linux.");
+            break;
+        }
+        try
+        {
+            File.AppendAllLines("/etc/environment", new string[] { "LC_ALL=C" });
+        }
+        catch (Exception e)
+        {
+            log_exception(e);
+        }
+        break;
+    default:
+        break;
+}
 
