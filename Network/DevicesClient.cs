a68eceea248ec8fd3e2ad9af9ebfe53e3cdf82c7
Dynesshely <catrol@qq.com>
2023-04-24 00:41:32 +0800

    🧩 Refactor(Network): Depart client part in devices network to `DevicesClient.cs`

--- Network/DevicesClient.cs
+++ Network/DevicesClient.cs
@@ -1,65 +1,246 @@
-﻿using KitX_Dashboard.Interfaces.Network;
+﻿using Common.BasicHelper.Utils.Extensions;
+using KitX_Dashboard.Interfaces.Network;
+using KitX_Dashboard.Managers;
+using Serilog;
 using System;
+using System.Net.Sockets;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace KitX_Dashboard.Network;
 
 internal class DevicesClient : IKitXClient<DevicesClient>
 {
+    private static TcpClient? client = null;
+
+    private static bool keepReceiving = false;
+
+    private static ClientStatus status = ClientStatus.Unknown;
+
+    public static string ServerAddress { get; set; } = string.Empty;
+
+    public static int ServerPort { get; set; } = 0;
+
+    public static ClientStatus Status
+    {
+        get => status;
+        set
+        {
+            status = value;
+
+            if (status == ClientStatus.Errored)
+                DevicesManager.Watch4MainDevice();
+        }
+    }
+
+    public static Action<byte[], int?>? onReceive = null;
+
+    public DevicesClient SetServerAddress(string address)
+    {
+        ServerAddress = address;
+
+        return this;
+    }
+
+    public DevicesClient SetServerPort(int port)
+    {
+        if (port <= 0 || port >= 65536)
+            throw new ArgumentOutOfRangeException(nameof(port), "0 < port < 65536");
+
+        ServerPort = port;
+
+        return this;
+    }
+
+    public async void Receive()
+    {
+        var location = $"{nameof(DevicesClient)}.{nameof(Receive)}";
+
+        if (client is null) return;
+
+        var stream = client?.GetStream();
+
+        if (stream is null) return;
+
+        var buffer = new byte[Program.Config.Web.SocketBufferSize];  //  Default 10 MB buffer
+
+        try
+        {
+            while (keepReceiving)
+            {
+                if (buffer is null) break;
+
+                var length = await stream.ReadAsync(buffer);
+
+                if (length > 0)
+                {
+                    var msg = buffer.ToUTF8(0, length);
+
+                    Log.Information($"Receive from Host: {msg}");
+                }
+                else
+                {
+                    keepReceiving = false;
+                    break;
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Log.Error(e, $"In {location}: {e.Message}");
+
+            Status = ClientStatus.Errored;
+        }
+
+        stream.CloseAndDispose();
+
+        client?.CloseAndDispose();
+    }
+
     public async Task<DevicesClient> Connect()
     {
-        await Task.Run(() => { });
+        var location = $"{nameof(DevicesClient)}.{nameof(Connect)}";
+
+        if (client is null) return this;
+
+        Status = ClientStatus.Connecting;
+
+        await TasksManager.RunTaskAsync(async () =>
+        {
+            try
+            {
+                await client.ConnectAsync(ServerAddress, ServerPort);
+
+                new Thread(Receive).Start();
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, $"In {location}: {ex.Message}");
+
+                await Stop();
+
+                Status = ClientStatus.Errored;
+            }
+
+        }, location);
+
+        Status = ClientStatus.Running;
 
         return this;
     }
 
     public async Task<DevicesClient> Disconnect()
     {
-        await Task.Run(() => { });
+        var location = $"{nameof(DevicesClient)}.{nameof(Disconnect)}";
+
+        Status = ClientStatus.Disconnecting;
+
+        await TasksManager.RunTaskAsync(() =>
+        {
+            keepReceiving = false;
+
+            client?.Close();
+
+        }, location);
+
+        Status = ClientStatus.Pending;
 
         return this;
     }
 
     public DevicesClient OnReceive(Action<byte[], int?> action)
     {
+        onReceive = action;
+
         return this;
     }
 
     public async Task<DevicesClient> Send(byte[] content)
     {
-        await Task.Run(() => { });
+        var location = $"{nameof(DevicesClient)}.{nameof(Send)}";
+
+        await TasksManager.RunTaskAsync(async () =>
+        {
+            try
+            {
+                var stream = client?.GetStream();
+
+                if (stream is null) return;
+
+                stream.Write(content, 0, content.Length);
+                stream.Flush();
+
+                Log.Information($"Sent Message to Host, msg: {content.ToUTF8()}");
+            }
+            catch (Exception e)
+            {
+                Log.Error(e, $"In {location}: {e.Message}");
+
+                await Stop();
+
+                Status = ClientStatus.Errored;
+            }
+        }, location);
 
         return this;
     }
 
+    private static void Init()
+    {
+        keepReceiving = true;
+
+        client = new();
+    }
+
     public async Task<DevicesClient> Start()
     {
-        await Task.Run(() => { });
+        var location = $"{nameof(DevicesClient)}.{nameof(Start)}";
+
+        await TasksManager.RunTaskAsync(async () =>
+        {
+            Status = ClientStatus.Pending;
+
+            Init();
+
+            await Connect();
+
+        }, location);
 
         return this;
     }
 
     public async Task<DevicesClient> Stop()
     {
-        await Task.Run(() => { });
+        var location = $"{nameof(DevicesClient)}.{nameof(Stop)}";
+
+        await TasksManager.RunTaskAsync(async () =>
+        {
+            keepReceiving = false;
+
+            await Disconnect();
+
+        }, location);
 
         return this;
     }
 
     public async Task<DevicesClient> Restart()
     {
-        await Task.Run(async () =>
+        var location = $"{nameof(DevicesClient)}.{nameof(Restart)}";
+
+        await TasksManager.RunTaskAsync(async () =>
         {
+            await Stop();
+
             await Start();
 
-            await Stop();
-        });
+        }, location);
 
         return this;
     }
 
     public void Dispose()
     {
-
+        client?.Dispose();
     }
 }
--- Network/DevicesServer.cs
+++ Network/DevicesServer.cs
@@ -1,4 +1,5 @@
-﻿using KitX_Dashboard.Data;
+﻿using Common.BasicHelper.Utils.Extensions;
+using KitX_Dashboard.Data;
 using KitX_Dashboard.Interfaces.Network;
 using KitX_Dashboard.Managers;
 using KitX_Dashboard.Services;
@@ -7,7 +8,6 @@ using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -15,358 +15,204 @@ namespace KitX_Dashboard.Network;
 
 internal class DevicesServer : IKitXServer<DevicesServer>
 {
-    public NetworkType Type { get; set; } = NetworkType.Unknown;
+    private static TcpListener? listener = null;
 
-    internal Thread? acceptDeviceThread;
-    internal Thread? receiveMessageThread;
-    internal TcpClient? DevicesHost;
-    internal TcpListener? listener;
-    internal bool keepListen = true;
+    private static bool keepListen = true;
 
-    public readonly Dictionary<string, TcpClient> clients = new();
+    private static readonly Dictionary<string, TcpClient> clients = new();
 
-    /// <summary>
-    /// 建立主控网络
-    /// </summary>
-    internal void BuildServer()
+    private static Action<byte[], int?, string>? onReceive = null;
+
+    private static ServerStatus status = ServerStatus.Unknown;
+
+    public static ServerStatus Status
     {
-        listener = new(IPAddress.Any, 0);
-        acceptDeviceThread = new(AcceptClient);
+        get => status;
+        set
+        {
+            status = value;
 
-        listener.Start();
-
-        var port = ((IPEndPoint)listener.LocalEndpoint).Port; // 取服务端口号
-
-        GlobalInfo.DeviceServerPort = port; // 全局端口号标明
-        GlobalInfo.ServerBuildTime = DateTime.UtcNow;
-        GlobalInfo.IsMainMachine = true;
-
-        Log.Information($"DevicesServer Port: {port}");
-
-        acceptDeviceThread.Start();
-
-        EventService.Invoke(nameof(EventService.DevicesServerPortChanged));
-    }
-
-    /// <summary>
-    /// 取消建立主控网络
-    /// </summary>
-    internal void CancleBuildServer()
-    {
-        GlobalInfo.IsMainMachine = false;
-        GlobalInfo.DeviceServerPort = -1;
-
-        EventService.Invoke(nameof(EventService.DevicesServerPortChanged));
-
-        keepListen = false;
-        acceptDeviceThread?.Join();
-
-        DevicesHost?.Close();
-        DevicesHost?.Dispose();
-
-        DevicesManager.Watch4MainDevice();  //  取消建立之后重新寻找并加入主控网络
+            if (status == ServerStatus.Errored)
+                DevicesManager.Watch4MainDevice();
+        }
     }
 
     /// <summary>
     /// 接收客户端
     /// </summary>
-    internal void AcceptClient()
+    private static void AcceptClient()
     {
+        var location = $"{nameof(DevicesServer)}.{nameof(AcceptClient)}";
+
         try
         {
-            while (keepListen)
+            while (keepListen && listener is not null)
             {
-                if (listener != null && listener.Pending())
+                if (listener.Pending())
                 {
-                    TcpClient client = listener.AcceptTcpClient();
-                    if (client.Client.RemoteEndPoint is IPEndPoint endpoint)
-                    {
-                        clients.Add(endpoint.ToString(), client);
+                    var client = listener.AcceptTcpClient();
 
-                        Log.Information($"New device connection: {endpoint}");
+                    if (client.Client.RemoteEndPoint is not IPEndPoint endpoint) continue;
 
-                        // 新建并运行接收消息线程
-                        new Thread(() =>
-                        {
-                            try
-                            {
-                                ReceiveMessage(client);
-                            }
-                            catch (Exception e)
-                            {
-                                Log.Error(e,
-                                    "In DevicesServer.AcceptClient().ReceiveMessageFromHost()");
-                            }
-                        }).Start();
-                    }
-                }
-                else
-                {
-                    Thread.Sleep(100);
+                    clients.Add(endpoint.ToString(), client);
+
+                    Log.Information($"New device connection: {endpoint}");
+
+                    ReceiveMessage(client);
                 }
             }
         }
         catch (Exception ex)
         {
-            Log.Error($"In AcceptClient() : {ex.Message}");
+            Log.Error(ex, $"In {nameof(location)}: {ex.Message}");
         }
-    }
-
-    /// <summary>
-    /// 向客户端发送消息
-    /// </summary>
-    /// <param name="msg">消息内容</param>
-    /// <param name="client">客户端</param>
-    internal void SendMessage(string msg, string client)
-    {
-        if (clients.ContainsKey(client))
-            clients[client].Client.Send(Encoding.UTF8.GetBytes(msg));
     }
 
     /// <summary>
     /// 接收客户端消息
     /// </summary>
     /// <param name="obj">TcpClient</param>
-    private async void ReceiveMessage(object obj)
+    private static void ReceiveMessage(TcpClient client)
     {
-        TcpClient? client = obj as TcpClient;
+        var location = $"{nameof(DevicesServer)}.{nameof(ReceiveMessage)}";
+
         IPEndPoint? endpoint = null;
         NetworkStream? stream = null;
 
-        try
+        new Thread(async () =>
         {
-            endpoint = client?.Client.RemoteEndPoint as IPEndPoint;
-            stream = client?.GetStream();
-
-            while (keepListen && stream != null)
+            try
             {
-                byte[] data = new byte[Program.Config.Web.SocketBufferSize];
+                endpoint = client?.Client.RemoteEndPoint as IPEndPoint;
 
-                //如果远程主机已关闭连接,Read将立即返回零字节
-                //int length = await stream.ReadAsync(data, 0, data.Length);
+                stream = client?.GetStream();
 
-                int length = await stream.ReadAsync(data);
+                if (endpoint is null || stream is null) return;
 
-                if (length > 0)
+                while (keepListen)
                 {
-                    string msg = Encoding.UTF8.GetString(data, 0, length);
+                    var buffer = new byte[Program.Config.Web.SocketBufferSize];
 
-                    Log.Information($"From: {endpoint}\tReceive: {msg}");
+                    var length = await stream.ReadAsync(buffer);
 
+                    if (length > 0)
+                    {
+                        onReceive?.Invoke(buffer, length, endpoint.ToString());
 
-                    //发送到其他客户端
-                    //foreach (KeyValuePair<string, TcpClient> kvp in clients)
-                    //{
-                    //    if (kvp.Value != client)
-                    //    {
-                    //        byte[] writeData = Encoding.UTF8.GetBytes(msg);
-                    //        NetworkStream writeStream = kvp.Value.GetStream();
-                    //        writeStream.Write(writeData, 0, writeData.Length);
-                    //    }
-                    //}
-                }
-                else
-                {
+                        var msg = buffer.ToUTF8(0, length);
 
-                    break; //客户端断开连接 跳出循环
+                        Log.Information($"From: {endpoint}\tReceive: {msg}");
+                    }
+                    else break; // 客户端断开连接 跳出循环
                 }
             }
-        }
-        catch (Exception ex)
-        {
-            Log.Error($"Error: In ReceiveMessageFromHost() : {ex.Message}");
-            Log.Information($"Connection broke from: {endpoint}");
-
-            //Read是阻塞方法 客户端退出是会引发异常 释放资源 结束此线程
-        }
-        finally
-        {
-            //释放资源
-            if (endpoint != null)
+            catch (Exception ex)
             {
-                clients.Remove(endpoint.ToString());
+                Log.Error($"In {location}: {ex.Message}");
+                Log.Information($"Connection broke from: {endpoint}");
             }
-
-            stream?.Close();
-            stream?.Dispose();
-            client?.Dispose();
-        }
-    }
-
-    /// <summary>
-    /// 向所有客户端广播消息
-    /// </summary>
-    /// <param name="msg">消息</param>
-    internal void BroadCastMessage(string msg, Func<TcpClient, bool>? pattern)
-    {
-        foreach (var client in clients)
-        {
-            if (pattern is not null && pattern.Invoke(client.Value))
-                client.Value.Client.Send(Encoding.UTF8.GetBytes(msg));
-            else client.Value.Client.Send(Encoding.UTF8.GetBytes(msg));
-        }
-    }
-
-    /// <summary>
-    /// 加入主控网络
-    /// </summary>
-    /// <param name="serverAddress">主控地址</param>
-    /// <param name="serverPort">主控端口</param>
-    internal void AttendServer(string serverAddress, int serverPort)
-    {
-        try
-        {
-            DevicesHost?.Connect(serverAddress, serverPort);
-
-            keepListen = true;
-
-            receiveMessageThread = new(ReceiveMessageFromHost);
-
-            receiveMessageThread.Start();
-
-            Log.Information($"Attending Server -> {serverAddress}:{serverPort}");
-        }
-        catch (Exception ex)
-        {
-            var location = $"{nameof(DevicesServer)}.{nameof(AttendServer)}";
-
-            Log.Error(ex, $"In {location}: {ex.Message}");
-        }
-    }
-
-    /// <summary>
-    /// 向主控发送消息
-    /// </summary>
-    /// <param name="msg">消息内容</param>
-    internal void SendMessageToHost(string msg)
-    {
-        try
-        {
-            var stream = DevicesHost?.GetStream();
-
-            if (stream is null) return;
-
-            var data = Encoding.UTF8.GetBytes(msg);
-
-            stream.Write(data, 0, data.Length);
-            stream.Flush();
-
-            //DevicesHost?.Client.Send(Encoding.UTF8.GetBytes(msg));
-
-            Log.Information($"Sent Message to Host, msg: {msg}");
-        }
-        catch (Exception e)
-        {
-            var location = $"{nameof(DevicesServer)}.{nameof(SendMessageToHost)}";
-
-            Log.Error(e, $"In {location}: {e.Message}");
-
-            Program.WebManager?.Restart(restartPluginsServer: false);
-        }
-    }
-
-    /// <summary>
-    /// 从主控接收消息
-    /// </summary>
-    internal async void ReceiveMessageFromHost()
-    {
-        if (DevicesHost is null) return;
-
-        var stream = DevicesHost?.GetStream();
-
-        if (stream is null) return;
-
-        var buffer = new byte[Program.Config.Web.SocketBufferSize];  //  Default 10 MB buffer
-
-        try
-        {
-            while (keepListen)
+            finally
             {
-
-                if (buffer is null) break;
-
-                var length = await stream.ReadAsync(buffer);
-
-                if (length > 0)
+                if (endpoint is not null)
                 {
-                    var msg = Encoding.UTF8.GetString(buffer, 0, length);
+                    clients.Remove(endpoint.ToString());
+                }
 
-                    Log.Information($"Receive from Host: {msg}");
-                }
-                else
-                {
-                    keepListen = false;
-                    break;
-                }
+                stream?.CloseAndDispose();
+
+                client.Dispose();
             }
-
-            stream.Close();
-            stream.Dispose();
-
-            Log.Information($"Closing `{nameof(ReceiveMessageFromHost)}` thread.");
-
-            Program.WebManager?.Restart(restartPluginsServer: false);
-        }
-        catch (Exception e)
-        {
-            var location = $"{nameof(DevicesServer)}.{nameof(ReceiveMessageFromHost)}";
-            Log.Error(e, $"In {location}: {e.Message}");
-
-            stream.Close();
-            stream.Dispose();
-
-            DevicesHost?.Close();
-            DevicesHost?.Dispose();
-
-            Program.WebManager?.Restart(restartPluginsServer: false);
-        }
-    }
-
-    public void Dispose()
-    {
-        GC.SuppressFinalize(this);
+        }).Start();
     }
 
     public async Task<DevicesServer> Broadcast(byte[] content)
     {
-        await Task.Run(() => { });
+        await Task.Run(() =>
+        {
+            foreach (var client in clients)
+                client.Value.Client.Send(content);
+        });
 
         return this;
     }
 
     public async Task<DevicesServer> BroadCast(byte[] content, Func<TcpClient, bool>? pattern)
     {
-        await Task.Run(() => { });
+        await Task.Run(() =>
+        {
+            foreach (var client in clients)
+            {
+                if (pattern is not null && pattern.Invoke(client.Value))
+                    client.Value.Client.Send(content);
+                else client.Value.Client.Send(content);
+            }
+        });
 
         return this;
     }
 
     public async Task<DevicesServer> Send(byte[] content, string target)
     {
-        await Task.Run(() => { });
+        await Task.Run(() =>
+        {
+            if (clients.ContainsKey(target))
+                clients[target].Client.Send(content);
+        });
 
         return this;
     }
 
     public DevicesServer OnReceive(Action<byte[], int?, string> action)
     {
+        onReceive = action;
+
         return this;
     }
 
     public async Task<DevicesServer> Start()
     {
+        var location = $"{nameof(DevicesServer)}.{nameof(Start)}";
+
+        Status = ServerStatus.Pending;
+
+        keepListen = true;
+
         await TasksManager.RunTaskAsync(() =>
         {
-            DevicesHost = new();
-        }, $"{nameof(DevicesServer)}.{nameof(Start)}");
+            clients.Clear();
+
+            listener = new(IPAddress.Any, 0);
+            listener.Start();
+
+            var port = ((IPEndPoint)listener.LocalEndpoint).Port; // 取服务端口号
+
+            GlobalInfo.DeviceServerPort = port; // 全局端口号标明
+            GlobalInfo.ServerBuildTime = DateTime.UtcNow;
+            GlobalInfo.IsMainMachine = true;
+
+            Log.Information($"DevicesServer Port: {port}");
+
+            EventService.Invoke(nameof(EventService.DevicesServerPortChanged));
+
+            Status = ServerStatus.Running;
+
+            new Thread(AcceptClient).Start();
+
+        }, location);
 
         return this;
     }
 
     public async Task<DevicesServer> Stop()
     {
-        await Task.Run(() =>
+        var location = $"{nameof(DevicesServer)}.{nameof(Stop)}";
+
+        Status = ServerStatus.Stopping;
+
+        await TasksManager.RunTaskAsync(() =>
         {
+            listener?.Stop();
+
             keepListen = false;
 
             foreach (KeyValuePair<string, TcpClient> item in clients)
@@ -375,23 +221,33 @@ internal class DevicesServer : IKitXServer<DevicesServer>
                 item.Value.Dispose();
             }
 
-            acceptDeviceThread?.Join();
+            GlobalInfo.IsMainMachine = false;
+            GlobalInfo.DeviceServerPort = -1;
 
-            DevicesHost?.Close();
-            DevicesHost?.Dispose();
-        });
+            EventService.Invoke(nameof(EventService.DevicesServerPortChanged));
+
+            Status = ServerStatus.Pending;
+
+        }, location);
 
         return this;
     }
 
     public async Task<DevicesServer> Restart()
     {
-        await Task.Run(async () =>
+        var location = $"{nameof(DevicesServer)}.{nameof(Restart)}";
+
+        await TasksManager.RunTaskAsync(async () =>
         {
             await Start();
             await Stop();
-        });
+        }, location);
 
         return this;
+    }
+
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
     }
 }
